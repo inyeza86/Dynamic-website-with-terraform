9594347c4854db2b5cf7aa2da956de90acb674d3
inyeza <inyeza1986@gmail.com>
2024-12-20 01:42:29 -0600

    create eice

--- ec2-instance-connect-ep.tf
+++ ec2-instance-connect-ep.tf
@@ -2,11 +2,11 @@
 # Please review these resources and move them into your main configuration files.
 
 # __generated__ by Terraform from "eice-03ecab3f69e176649"
-# resource "aws_ec2_instance_connect_endpoint" "eice" {
-#   preserve_client_ip = false
-#   security_group_ids = [aws_security_group.ec2-instance-connect-endpoint-sg.id]
-#   subnet_id          = aws_subnet.my_private_data_subnet_az1.id
-#   tags = {
-#     Name = "${var.environment}-EICE"
-#   }
-# }
+resource "aws_ec2_instance_connect_endpoint" "eice" {
+  preserve_client_ip = false
+  security_group_ids = [aws_security_group.ec2-instance-connect-endpoint-sg.id]
+  subnet_id          = aws_subnet.my_private_data_subnet_az1.id
+  tags = {
+    Name = "${var.environment}-EICE"
+  }
+}
